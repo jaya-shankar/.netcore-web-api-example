e900da4891ff73ea91b6b195f275f3ed822e5854
Rithin Chalumuri <rithin.chalumuri@pinewood.co.uk>
2021-05-30 00:04:31 +0100

    Created Initial Pokedex.API Project + PokemonController

---
+++ Pokedex/Pokedex.API/Controllers/Pokemon/PokemonController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pokedex.API.Controllers.Pokemon
+{
+    [ApiController]
+    [Route("pokemon")]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            return Ok(name);
+        }
+
+        [HttpGet("translate/{name}")]
+        public IActionResult GetTranslatedPokemon(string name)
+        {
+            return Ok(name);
+        }
+    }
+}
